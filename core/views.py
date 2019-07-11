8dc95da55d534dd5de427e2731f9ea4ef2a98aca
DuncanZim <duncanfmoyo@gmail.com>
2019-07-11 16:34:47 +0200

    order summary view

--- core/views.py
+++ core/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Item, Order, OrderItem
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, View, DetailView
 from django.utils import timezone
 from django.contrib import messages
+from django.core.exceptions import ObjectDoesNotExist
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
 def products(request):
@@ -22,11 +25,25 @@ class HomeView(ListView):
     template_name = 'home.html'
 
 
+class OrderSummaryView(LoginRequiredMixin, View):
+    def get(self, *args, **kwargs):
+        try:
+            order = Order.objects.get(user=self.request.user, ordered=False)
+            context = {
+                'object': order
+            }
+            return render(self.request, 'order_summary.html', context)
+        except ObjectDoesNotExist:
+            messages.error(self.request, 'You do not have an active order')
+            return redirect('/')
+
+
 class ItemDetailView(DetailView):
     model = Item
     template_name = 'product.html'
 
 
+@login_required
 def add_to_cart(request, slug):
     item = get_object_or_404(Item, slug=slug)
     order_item, created = OrderItem.objects.get_or_create(item=item, user=request.user, ordered=False)
@@ -34,24 +51,25 @@ def add_to_cart(request, slug):
 
     if order_qs.exists():
         order = order_qs[0]
-
+        # check if the order item is in the order
         if order.items.filter(item__slug=item.slug).exists():
             order_item.quantity += 1
             order_item.save()
             messages.info(request, 'This item quantity was updated')
-            return redirect('core:product', slug=slug)
+            return redirect('core:order-summary')
         else:
             order.items.add(order_item)
             messages.info(request, 'This item was added to your cart')
-            return redirect('core:product', slug=slug)
+            return redirect('core:order-summary')
     else:
         ordered_date = timezone.now()
         order = Order.objects.create(user=request.user, ordered_date=ordered_date)
         order.items.add(order_item)
         messages.info(request, 'This item was added to your cart')
-        return redirect('core:product', slug=slug)
+        return redirect('core:order-summary')
 
 
+@login_required
 def remove_from_cart(request, slug):
     item = get_object_or_404(Item, slug=slug)
     order_qs = Order.objects.filter(
@@ -69,8 +87,8 @@ def remove_from_cart(request, slug):
                 ordered=False
             )[0]
             order.items.remove(order_item)
-            messages.info(request, 'This item was removed to your cart')
-            return redirect('core:product', slug=slug)
+            messages.info(request, 'This item was removed from your cart.')
+            return redirect('core:order-summary')
         else:
             messages.info(request, 'This item was not in your cart')
             return redirect('core:product', slug=slug)
@@ -79,6 +97,36 @@ def remove_from_cart(request, slug):
         return redirect('core:product', slug=slug)
 
 
+@login_required
+def remove_single_item_from_cart(request, slug):
+    item = get_object_or_404(Item, slug=slug)
+    order_qs = Order.objects.filter(
+        user=request.user,
+        ordered=False
+    )
+
+    if order_qs.exists():
+        order = order_qs[0]
+
+        if order.items.filter(item__slug=item.slug).exists():
+            order_item = OrderItem.objects.filter(
+                item=item,
+                user=request.user,
+                ordered=False
+            )[0]
+            if order_item.quantity > 1:
+                order_item.quantity -= 1
+                order_item.save()
+            else:
+                order.items.remove(order_item)
+            messages.info(request, 'This item quantity was updated')
+            return redirect('core:order-summary')
+        else:
+            messages.info(request, 'This item was not in your cart')
+            return redirect('core:product', slug=slug)
+    else:
+        messages.info(request, 'You do not have an active order')
+        return redirect('core:product', slug=slug)
 
 
 
