452eb633f7ccdf6f36072548fc3b06575fbde414
DuncanZim <duncanfmoyo@gmail.com>
2019-07-19 06:00:59 +0200

    managing refunds

--- core/urls.py
+++ core/urls.py
@@ -3,7 +3,7 @@ from .views import (HomeView, CheckoutView,
                     ItemDetailView, add_to_cart,
                     remove_from_cart, OrderSummaryView,
                     remove_single_item_from_cart, PaymentView,
-                    AddCouponView)
+                    AddCouponView, RequestRefundView)
 
 
 app_name = 'core'
@@ -18,4 +18,5 @@ urlpatterns = [
     path('remove-from-cart/<slug>/', remove_from_cart, name='remove-from-cart'),
     path('remove-item-from-cart/<slug>/', remove_single_item_from_cart, name='remove-single-item-from-cart'),
     path('payment/<payment_option>/', PaymentView.as_view(), name='payment'),
+    path('request-refund/', RequestRefundView.as_view(), name='request-refund'),
 ]
